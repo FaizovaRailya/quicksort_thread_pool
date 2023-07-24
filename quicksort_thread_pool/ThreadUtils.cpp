26e644450061f3e090ff233fe33fd7b14f888f92
Раиля <119114475+FaizovaRailya@users.noreply.github.com>
2023-07-24 17:34:11 +0500

    Update ThreadUtils.cpp

--- quicksort_thread_pool/ThreadUtils.cpp
+++ quicksort_thread_pool/ThreadUtils.cpp
@@ -5,17 +5,17 @@
 
 template<class T> void BlockedQueue<T>::push(T& item){
     std::lock_guard <std::mutex> l(m_locker);
-    // обычный потокобезопасный push
+    // РѕР±С‹С‡РЅС‹Р№ РїРѕС‚РѕРєРѕР±РµР·РѕРїР°СЃРЅС‹Р№ push
     m_task_queue.push(std::move(item));
-    // делаем оповещение, чтобы поток, вызвавший
-    // pop проснулся и забрал элемент из очереди
+    // РґРµР»Р°РµРј РѕРїРѕРІРµС‰РµРЅРёРµ, С‡С‚РѕР±С‹ РїРѕС‚РѕРє, РІС‹Р·РІР°РІС€РёР№
+    // pop РїСЂРѕСЃРЅСѓР»СЃСЏ Рё Р·Р°Р±СЂР°Р» СЌР»РµРјРµРЅС‚ РёР· РѕС‡РµСЂРµРґРё
     m_notifier.notify_one();
 }
 
 template<class T> void BlockedQueue<T>::pop(T& item){
     std::unique_lock < std::mutex > l(m_locker);
     if (m_task_queue.empty())
-        // ждем, пока вызовут push
+        // Р¶РґРµРј, РїРѕРєР° РІС‹Р·РѕРІСѓС‚ push
         m_notifier.wait(l, [this] {
         return !m_task_queue.empty();
             });
@@ -26,7 +26,7 @@ template<class T> void BlockedQueue<T>::pop(T& item){
 template<class T> bool BlockedQueue<T>::fast_pop(T& item){
     std::lock_guard < std::mutex > l(m_locker);
     if (m_task_queue.empty())
-        // просто выходим
+        // РїСЂРѕСЃС‚Рѕ РІС‹С…РѕРґРёРј
         return false;
     item = std::move(m_task_queue.front());
     m_task_queue.pop();
@@ -43,16 +43,16 @@ ThreadPool::~ThreadPool() {
     stop();
 }
 
-void ThreadPool::start() {     //Метод старта потоков
+void ThreadPool::start() {     //РњРµС‚РѕРґ СЃС‚Р°СЂС‚Р° РїРѕС‚РѕРєРѕРІ
     for (int i = 0; i < m_thread_count; i++) {
         m_threads.emplace_back(&ThreadPool::threadFunc, this, i);
     }
 }
 
-void ThreadPool::stop(){    //Метод завершения потока
+void ThreadPool::stop(){    //РњРµС‚РѕРґ Р·Р°РІРµСЂС€РµРЅРёСЏ РїРѕС‚РѕРєР°
     for (int i = 0; i < m_thread_count; i++) {
-        // кладем задачу-пустышку в каждую очередь
-        // для завершения потока
+        // РєР»Р°РґРµРј Р·Р°РґР°С‡Сѓ-РїСѓСЃС‚С‹С€РєСѓ РІ РєР°Р¶РґСѓСЋ РѕС‡РµСЂРµРґСЊ
+        // РґР»СЏ Р·Р°РІРµСЂС€РµРЅРёСЏ РїРѕС‚РѕРєР°
         task_type empty_task;
         m_thread_queues[i].push(empty_task);
     }
@@ -62,37 +62,37 @@ void ThreadPool::stop(){    //Метод завершения потока
 }
 
 res_type ThreadPool::push_task(FuncType f, std::vector<int>& arr, int l, int r, bool enable, ThreadPool& tp, int multi_size) {
-    // вычисляем индекс очереди, куда положим задачу
+    // РІС‹С‡РёСЃР»СЏРµРј РёРЅРґРµРєСЃ РѕС‡РµСЂРµРґРё, РєСѓРґР° РїРѕР»РѕР¶РёРј Р·Р°РґР°С‡Сѓ
     int queue_to_push = m_index++ % m_thread_count;
-// формируем функтор
+// С„РѕСЂРјРёСЂСѓРµРј С„СѓРЅРєС‚РѕСЂ
     task_type task([=, &arr, &tp]() { f(arr, l, r, enable, tp, multi_size); });
     auto res = task.get_future();
-    // кладем в очередь
+    // РєР»Р°РґРµРј РІ РѕС‡РµСЂРµРґСЊ
     m_thread_queues[queue_to_push].push(task);
     return res;
 }
 
 void ThreadPool::threadFunc(int qindex) {
     while (true) {
-        // обработка очередной задачи
+        // РѕР±СЂР°Р±РѕС‚РєР° РѕС‡РµСЂРµРґРЅРѕР№ Р·Р°РґР°С‡Рё
         task_type task_to_do;
         bool res;
         int i = 0;
         for (; i < m_thread_count; i++) {
-            // попытка быстро забрать задачу из любой очереди, начиная со своей
+            // РїРѕРїС‹С‚РєР° Р±С‹СЃС‚СЂРѕ Р·Р°Р±СЂР°С‚СЊ Р·Р°РґР°С‡Сѓ РёР· Р»СЋР±РѕР№ РѕС‡РµСЂРµРґРё, РЅР°С‡РёРЅР°СЏ СЃРѕ СЃРІРѕРµР№
             if (res = m_thread_queues[(qindex + i) % m_thread_count].fast_pop(task_to_do))
                 break;
         }
         if (!res) {
-            // вызываем блокирующее получение очереди
+            // РІС‹Р·С‹РІР°РµРј Р±Р»РѕРєРёСЂСѓСЋС‰РµРµ РїРѕР»СѓС‡РµРЅРёРµ РѕС‡РµСЂРµРґРё
             m_thread_queues[qindex].pop(task_to_do);
         }
         else if (!task_to_do.valid()) {
-            // чтобы не допустить зависания потока кладем обратно задачу-пустышку
+            // С‡С‚РѕР±С‹ РЅРµ РґРѕРїСѓСЃС‚РёС‚СЊ Р·Р°РІРёСЃР°РЅРёСЏ РїРѕС‚РѕРєР° РєР»Р°РґРµРј РѕР±СЂР°С‚РЅРѕ Р·Р°РґР°С‡Сѓ-РїСѓСЃС‚С‹С€РєСѓ
             m_thread_queues[(qindex + i) % m_thread_count].push(task_to_do);
         }
         if (!task_to_do.valid()) { return; }
-        // выполняем задачу
+        // РІС‹РїРѕР»РЅСЏРµРј Р·Р°РґР°С‡Сѓ
         task_to_do();
     }
 }
